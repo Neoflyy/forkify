bf31cb7758a7d8eca7ff63fc68f75f6bad49fc6a
Neoflyy <sabadzandzava@gmail.com>
2023-01-24 20:42:44 +0400

    forkify

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'ba677178-4ba8-4b26-9f62-25d313a73037';
+export const MODAL_CLOSE_SEC = 2.5;
